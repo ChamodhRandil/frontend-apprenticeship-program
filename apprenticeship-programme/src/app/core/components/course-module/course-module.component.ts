d6796f438bcb499705f882cde3e8d98e62072484
Chamodh Karunarathna <chamodhrandil16@gmail.com>
2024-03-07 23:45:03 +0530

    Create project folder structure

---
+++ apprenticeship-programme/src/app/core/components/course-module/course-module.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-module',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-module.component.html',
+  styleUrl: './course-module.component.scss'
+})
+export class CourseModuleComponent {
+
+}
